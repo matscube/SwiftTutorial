0942b4853f347a5654cbcd487a7f506e43346d6a
Takanori Matsumoto <matscube@gmail.com>
2014-12-19 03:22:57 +0900

    Add TableViewCell class.

---
+++ TableView/TableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableView
+//
+//  Created by TakanoriMatsumoto on 2014/12/19.
+//  Copyright (c) 2014年 TakanoriMatsumoto. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
